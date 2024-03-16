ce06102dc6a4e653ec11005a7354f6e29fbe3570
Sharat Jacob <sharatjacob2@gmail.com>
2024-03-16 23:14:02 +0530

    weird things

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const circle1 = document.querySelector('.pendulum1');
 const circle2 = document.querySelector('.pendulum2');
 const circle3 = document.querySelector('.pendulum3');
-circle4 = document.querySelector('.decorstuff');
+const circle4 = document.querySelector('.decorstuff');
 
-let circleTop = 0; // Initial top position, centered vertically
-circle4.style.offsetDistance = circleTop + '%';
+// let circleTop = 0; // Initial top position, centered vertically
+// circle4.style.offsetDistance = circleTop + '%';
 
 // Event listener for mouse wheel scroll
 window.addEventListener('wheel', (event) => {
@@ -13,19 +13,21 @@ window.addEventListener('wheel', (event) => {
 
   // Move the circle based on the scroll direction
   if (scrollAmount > 0) {
-    circleTop += 0.5;
+    // circleTop += 0.5;
     circle1.style.transform = 'rotate(-60deg) translateX(10px) translateY(450px) ';
     circle2.style.transform = 'rotate(-60deg) translateX(310px) translateY(500px) ';
     circle3.style.transform = 'rotate(-60deg) translateX(560px) translateY(550px) ';
+    circle4.style.offsetDistance = '100%';
   } else {
     circle1.style.transform = 'rotate(60deg) translateX(-250px) translateY(-250px) ';
     circle2.style.transform = 'rotate(60deg) translateX(-250px) translateY(-250px) ';
     circle3.style.transform = 'rotate(60deg) translateX(-250px) translateY(-250px) ';
-    circleTop -= 0.5;
+    circle4.style.offsetDistance = '0%';
+    // circleTop -= 0.5;
   }
 
   // Update the circle's position
-  circle4.style.offsetDistance = circleTop + '%';
+  // circle4.style.offsetDistance = circleTop + '%';
 });
 
 
